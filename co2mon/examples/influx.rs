1f811672d2ccd479de5d99c5b5d8a47de415ae9c
LeoVerto <LeoVerto@users.noreply.github.com>
2019-07-19 02:51:52 +0200

    Fix function name style & reduce temperature accuracy

--- co2mon/examples/influx.rs
+++ co2mon/examples/influx.rs
@@ -5,19 +5,19 @@ fn main() -> Result<()> {
     let sensor = Sensor::open_default()?;
     
     match sensor.read() {
-        Ok(reading) => printData(reading),
+        Ok(reading) => print_data(reading),
         Err(e) => eprintln!("{}", e),
     }
     Ok(())
 }
 
-fn printData(reading: co2mon::Reading) {
+fn print_data(reading: co2mon::Reading) {
     let start = SystemTime::now();
     let since_the_epoch = start.duration_since(UNIX_EPOCH)
     .expect("Time went backwards").as_secs();
     
     println!(
-        "{:?}000000000,{:.4},{}",
+        "{:?}000000000,{:.1},{}",
         since_the_epoch,
         reading.temperature(),
         reading.co2(),
